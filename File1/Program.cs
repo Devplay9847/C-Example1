afc3590f59e83ff45b1f5c8684ab0984b31a6cd9
Devplay9847 <devendra.nishad.9847@gmail.com>
2022-04-28 13:02:47 +0530

    This modifictaion by UC1

--- File1/Program.cs
+++ File1/Program.cs
@@ -3,7 +3,14 @@ class Exampe1
 {
     public static void Main()
     {
-        Console.WriteLine("A simple c# program.");
+        int x;
+        int y;
+        x = 100;
+        Console.WriteLine("x contains " + x);
+        y = x / 2;
+        Console.WriteLine("Y contains x/2: ");
+        Console.WriteLine(y);
         Console.ReadLine();
+
     }
 }
